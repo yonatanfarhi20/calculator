bd8073fec2fbb0a5eb1a24c49b08d65fd6f22756
yonatanfarhi20 <yonatanfarhi1@gmail.com~>
2026-08-10 21:04:14 +0300

    create main

--- calculator.c
+++ calculator.c
@@ -7,7 +7,33 @@ void print_division(double, double);
 void calculate(double, double, char);
 
 int main(){
-    
+    int choice;
+    double a, b;
+    char op;
+
+    printf("Welcome to the Calculator!\n");
+
+    do {
+        printf("Please enter your expression (number operator number): ");
+        if (scanf("%lf %c %lf", &a, &op, &b) == 3) {
+            calculate(a, b, op);
+        } else {
+            printf("Error: Invalid input format.\n");
+            while (getchar() != '\n'); // clear the buffer
+        }
+
+        printf("\n1. Calculate another operation\n");
+        printf("Any other key to exit the system\n");
+        printf("Choose an option: ");
+        
+        if (scanf("%d", &choice) != 1 || choice != 1) {
+            printf("Exiting system. Goodbye!\n");
+            break;
+        }
+
+    } while (1);
+
+    return 0;
 }
 
 void print_addition(double a, double b) {
