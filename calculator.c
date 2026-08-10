aefe78d7a1b7d8751fd4024503129e02e654cdd8
yonatanfarhi20 <yonatanfarhi1@gmail.com~>
2026-08-10 19:17:56 +0300

    create func print_division

--- calculator.c
+++ calculator.c
@@ -3,6 +3,7 @@
 void print_addition(double, double);
 void print_subtraction(double, double);
 void print_multiplication(double, double);
+void print_division(double, double);
 
 int main(){
     
@@ -18,4 +19,12 @@ void print_subtraction(double a, double b) {
 
 void print_multiplication(double a, double b) {
     printf("%.2f\n", a * b);
+}
+
+void print_division(double a, double b) {
+    if (b == 0) {
+        printf("Error: This operation is undefined\n");
+    } else {
+        printf("%.2f\n", a / b);
+    }
 }
