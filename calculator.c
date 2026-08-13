23665429128d467796bed697aff07b7511bd59d8
yonatanfarhi20 <yonatanfarhi1@gmail.com~>
2026-08-14 00:12:47 +0300

    Add division function to calculator and update .gitignore to include .cursor directory. closes #5

--- calculator.c
+++ calculator.c
@@ -14,3 +14,12 @@ double multiply(double a, double b)
 {
     printf("%g\n", a * b);
 }
+
+void divide(double a, double b)
+{
+    if (b == 0) {
+        printf("This operation is not defined\n");
+        return;
+    }
+    printf("%g\n", a / b);
+}
